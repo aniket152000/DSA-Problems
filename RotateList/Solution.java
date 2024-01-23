edd54ff39c1deb40194ca971f7e80c62379aee27
aniket152000 <aniketghare24@gmail.com>
2024-01-23 22:24:08 +0530

    Roatate List solution

--- RotateList/Solution.java
+++ RotateList/Solution.java
@@ -2,29 +2,25 @@ package RotateList;
 
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head==null || head.next ==null || k==0 ) return head;
+        if (head == null || head.next == null || k == 0)
+            return head;
 
         ListNode curr = head;
-
         int size = 1;
-        while(curr.next!=null){
+        while (curr.next != null) {
             size++;
-            curr=curr.next;
+            curr = curr.next;
         }
-        
         curr.next = head;
-
         k = k % size;
         k = size - k;
 
-        while(k>0){
-            curr=curr.next;
+        while (k > 0) {
+            curr = curr.next;
             k--;
         }
-
-        head=curr.next;
+        head = curr.next;
         curr.next = null;
-
         return head;
     }
 }
